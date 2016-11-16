c693df88ac410688c2570318be2c8052587d8ef5
Andy <admin@tensensefinance.com>
2016-11-15 23:14:45 -0600

    Initial add

---
+++ index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+const _ = require('lodash'),
+    express = require('express'),
+    fs = require('fs'),
+    path = require('path'),
+    app = express(),
+    rootDir = 'C:\\Users\\Andy\\Torrents';
+
+app
+    .use(express.static(process.cwd())) // app public directory
+    .use(express.static(__dirname)) // module directory
+    .get('/files', (req, res) => {
+        const query = req.query.path || '',
+            currentDir = query ? path.join(rootDir, query) : rootDir;
+
+        fs.readdir(currentDir, (err, files) => {
+            if (err) {
+                throw err;
+            }
+
+            let data = [];
+            files.forEach(file => {
+                try {
+                    const isDirectory = fs.statSync(path.join(currentDir, file)).isDirectory(),
+                        dataFile = {
+                            Name: file,
+                            Path: path.join(query, file),
+                            IsDirectory: isDirectory
+                        };
+
+                    if (!isDirectory) {
+                        dataFile.Ext = path.extname(file);
+                    }
+
+                    data.push(dataFile);
+                }
+                catch (e) {
+                    console.log(e);
+                }
+            });
+
+            // sort by name
+            data = _.sortBy(data, f => {
+                return f.Name
+            });
+
+            res.json(data);
+        });
+    })
+    .get('/', (req, res) => {
+        res.redirect('lib/index.html');
+    })
+    .listen(8080, () => {
+        console.log(`Server running at http://localhost:8080`);
+    });
